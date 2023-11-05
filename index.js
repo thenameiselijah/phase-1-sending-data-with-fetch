cf34885c388757c4ece23f193f8cce1ba9882197
Elijah Oquendo <elijahoquendo420@gmail.com>
2023-11-04 20:10:40 -0400

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email
+  };
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(userData)
+  };
+
+  return fetch('http://localhost:3000/users', options)
+    .then(response => response.json())
+    .then(data => {
+      const id = data.id;
+      const idElement = document.createElement('p');
+      idElement.textContent = `New id: ${id}`;
+      document.body.appendChild(idElement);
+    })
+    .catch(error => {
+      const errorElement = document.createElement('p');
+      errorElement.textContent = `Error: ${error.message}`;
+      document.body.appendChild(errorElement);
+    });
+}
